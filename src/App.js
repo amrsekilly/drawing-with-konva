a7e5feb9143053218f7448f1ae937983486803b9
Amr Elsekilly <github@amrsekilly.com>
2018-12-04 03:27:26 +0200

    Trying out drawing in Konva

--- src/App.js
+++ src/App.js
@@ -1,26 +1,106 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { render } from "react-dom";
+import { Stage, Layer, Rect, Image, Group } from "react-konva";
+
+class Drawing extends Component {
+  state = {
+    isDrawing: false,
+    mode: "brush"
+  };
+
+  componentDidMount() {
+    const canvas = document.createElement("canvas");
+    canvas.width = 300;
+    canvas.height = 300;
+    const context = canvas.getContext("2d");
+
+    this.setState({ canvas, context });
+  }
+
+  handleMouseDown = () => {
+    console.log("mousedown");
+    this.setState({ isDrawing: true });
+
+    // TODO: improve
+    const stage = this.image.parent.parent;
+    this.lastPointerPosition = stage.getPointerPosition();
+  };
+
+  handleMouseUp = () => {
+    console.log("mouseup");
+    this.setState({ isDrawing: false });
+  };
+
+  handleMouseMove = () => {
+    // console.log('mousemove');
+    const { context, isDrawing, mode } = this.state;
+
+    if (isDrawing) {
+      console.log("drawing");
+
+      // TODO: Don't always get a new context
+      context.strokeStyle = "#df4b26";
+      context.lineJoin = "round";
+      context.lineWidth = 5;
+
+      if (mode === "brush") {
+        context.globalCompositeOperation = "source-over";
+      } else if (mode === "eraser") {
+        context.globalCompositeOperation = "destination-out";
+      }
+      context.beginPath();
+
+      var localPos = {
+        x: this.lastPointerPosition.x - this.image.x(),
+        y: this.lastPointerPosition.y - this.image.y()
+      };
+      console.log("moveTo", localPos);
+      context.moveTo(localPos.x, localPos.y);
+
+      // TODO: improve
+      const stage = this.image.parent.parent;
+
+      var pos = stage.getPointerPosition();
+      localPos = {
+        x: pos.x - this.image.x(),
+        y: pos.y - this.image.y()
+      };
+      console.log("lineTo", localPos);
+      context.lineTo(localPos.x, localPos.y);
+      context.closePath();
+      context.stroke();
+      this.lastPointerPosition = pos;
+      this.image.getLayer().draw();
+    }
+  };
+
+  render() {
+    const { canvas } = this.state;
+    console.log("canvas", canvas);
+
+    return (
+      <Image
+        image={canvas}
+        ref={node => (this.image = node)}
+        width={300}
+        height={300}
+        stroke="blue"
+        onMouseDown={this.handleMouseDown}
+        onMouseUp={this.handleMouseUp}
+        onMouseMove={this.handleMouseMove}
+      />
+    );
+  }
+}
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <Stage width={700} height={700}>
+        <Layer>
+          <Drawing />
+        </Layer>
+      </Stage>
     );
   }
 }
